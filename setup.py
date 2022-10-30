95538173ab0a78fb08264e75deec33e43ab9eb9a
Bruce McGrgor <bm3pk@virginia.edu>
2022-10-30 15:59:22 -0400

    updated the setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup (name='booklover',
+      version = '1.0.0',
+      description='the best books read by the bookclub',
+      url='https://github.com/bmcgregor22/bookclub.git',
+      author = 'Bruce McGreor',
+      license = 'GNU',
+      packages= ['src','tests','booklove'])
+       
+       
